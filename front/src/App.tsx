df3d845b456e6f56b7955405f23b9dba59347d49
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-03 20:23:01 +0300

    edit: rebuild routes with outlet

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,19 @@
 import { FC } from 'react'
 import { publicRoutes, SIGN_IN } from './routes'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
 
 const App: FC = () => {
   const isAuth = false
   return (
     <Routes>
-      {publicRoutes.map(({ path, Component }) => (
-        <Route key={path} path={path} element={<Component />} />
-      ))}
-      {!isAuth && <Route path="*" element={<Navigate to={SIGN_IN} />} />}
+      <Route path="*" element={<Navigate to={SIGN_IN} />} />
+      <Route path="/" element={<MainLayout />}>
+        {publicRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+        {!isAuth && <Route path="*" element={<Navigate to={SIGN_IN} />} />}
+      </Route>
     </Routes>
   )
 }
