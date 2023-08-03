6e4a22cd7dd0bb50f9a2f878dda2bc973e0992e9
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-03 21:13:26 +0300

    feat: add login & register form

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,6 +1,48 @@
 import { FC } from 'react'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Form, Input } from 'antd'
+import { Link } from 'react-router-dom'
+import { SIGN_UP } from '../routes'
+// const onFinish = (values: any) => {
+//   console.log('Success:', values)
+// }
+
+// const onFinishFailed = (errorInfo: any) => {
+//   console.log('Failed:', errorInfo)
+// }
 
 const Login: FC = () => {
-  return <div>login</div>
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+      // onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input.Password
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Sing In
+        </Button>
+        Or <Link to={SIGN_UP}>sign up!</Link>
+      </Form.Item>
+    </Form>
+  )
 }
 export default Login
--- front/src/pages/registration.tsx
+++ front/src/pages/registration.tsx
@@ -1,6 +1,41 @@
 import { FC } from 'react'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Form, Input } from 'antd'
+import { SIGN_IN } from '../routes'
+import { Link } from 'react-router-dom'
 
 const Registration: FC = () => {
-  return <div>Registration</div>
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{ remember: true }}
+      // onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input.Password
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Sign Up
+        </Button>
+        Or <Link to={SIGN_IN}>sing in!</Link>
+      </Form.Item>
+    </Form>
+  )
 }
 export default Registration
