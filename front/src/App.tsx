6f1b7a05b219a49c7372f4eb24b35840ffb45596
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-04 18:51:57 +0300

    feat: add context provider

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,33 @@
-import { FC } from 'react'
+import { FC, createContext, useState } from 'react'
 import { publicRoutes, SIGN_IN } from './routes'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import './scss/app.scss'
 
+interface IAuth {
+  auth: boolean
+  setAuth: (c: boolean) => void
+}
+export const AuthContext = createContext<IAuth>({
+  auth: false,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setAuth: () => {}
+})
+
 const App: FC = () => {
-  const isAuth = false
+  const [auth, setAuth] = useState<boolean>(false)
   return (
-    <Routes>
-      <Route path="*" element={<Navigate to={SIGN_IN} />} />
-      <Route path="/" element={<MainLayout />}>
-        {publicRoutes.map(({ path, Component }) => (
-          <Route key={path} path={path} element={<Component />} />
-        ))}
-        {!isAuth && <Route path="*" element={<Navigate to={SIGN_IN} />} />}
-      </Route>
-    </Routes>
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      <Routes>
+        <Route path="*" element={<Navigate to={SIGN_IN} />} />
+        <Route path="/" element={<MainLayout />}>
+          {publicRoutes.map(({ path, Component }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
+          {!auth && <Route path="*" element={<Navigate to={SIGN_IN} />} />}
+        </Route>
+      </Routes>
+    </AuthContext.Provider>
   )
 }
 
