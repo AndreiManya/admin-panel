03c99de9e93e80089b57dde741dc80c701598b8c
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-04 19:43:30 +0300

    feat: make log inputs controlled & add onSubmit

--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -1,29 +1,46 @@
-import { FC } from 'react'
+import { FC, useContext, useState } from 'react'
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import { Button, Form, Input } from 'antd'
 import { Link } from 'react-router-dom'
 import { SIGN_UP } from '../routes'
-// const onFinish = (values: any) => {
-//   console.log('Success:', values)
-// }
-
-// const onFinishFailed = (errorInfo: any) => {
-//   console.log('Failed:', errorInfo)
-// }
+import { AuthContext } from '../App'
+import { login } from '../http/userAPI'
 
 const Login: FC = () => {
+  const { setAuth } = useContext(AuthContext)
+  const [name, setName] = useState<string>('')
+  const [pass, setPass] = useState<string>('')
+  const onSubmit = (): void => {
+    const fetch = async (): Promise<void> => {
+      try {
+        await login(name, pass).then((e) => e)
+        setAuth(true)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetch()
+  }
   return (
     <Form
       name="normal_login"
       className="login-form"
       initialValues={{ remember: true }}
-      // onFinish={onFinish}
+      onFinish={onSubmit}
     >
       <Form.Item
         name="username"
         rules={[{ required: true, message: 'Please input your Username!' }]}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        <Input
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
       </Form.Item>
       <Form.Item
         name="password"
@@ -33,6 +50,10 @@ const Login: FC = () => {
           prefix={<LockOutlined className="site-form-item-icon" />}
           type="password"
           placeholder="Password"
+          value={pass}
+          onChange={(e) => {
+            setPass(e.target.value)
+          }}
         />
       </Form.Item>
 
