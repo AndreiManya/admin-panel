e3732be3b30e275fc9f3b8fccc42f607ff2227ff
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-04 20:05:19 +0300

    feat: add private links  to navbar

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -1,17 +1,36 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import { NavLink } from 'react-router-dom'
-import { SIGN_IN, SIGN_UP } from '../routes'
+import { AuthContext } from '../App'
+import { SIGN_IN, SIGN_UP, USERS_LIST } from '../routes'
 
 const Header: FC = () => {
+  const { auth, setAuth } = useContext(AuthContext)
+  const logout = (): void => {
+    setAuth(false)
+  }
   return (
     <header className="header">
       <nav className="nav">
-        <NavLink className="nav__link" to={SIGN_IN}>
-          Sign in
-        </NavLink>
-        <NavLink className="nav__link" to={SIGN_UP}>
-          Sign up
-        </NavLink>
+        {!auth && (
+          <>
+            <NavLink className="nav__link" to={SIGN_IN}>
+              Sign in
+            </NavLink>
+            <NavLink className="nav__link" to={SIGN_UP}>
+              Sign up
+            </NavLink>
+          </>
+        )}
+        {auth && (
+          <>
+            <NavLink className="nav__link" to={USERS_LIST}>
+              Users
+            </NavLink>
+            <NavLink className="nav__link" to={SIGN_IN} onClick={logout}>
+              Logout
+            </NavLink>
+          </>
+        )}
       </nav>
     </header>
   )
