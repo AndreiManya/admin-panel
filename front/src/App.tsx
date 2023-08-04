2d568b4aab39f9b1920a2794f632368992ddfc84
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-04 19:55:31 +0300

    feat: add spinner context

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,30 +3,35 @@ import { publicRoutes, SIGN_IN } from './routes'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import './scss/app.scss'
+import { IAuth, ILoading } from './@types/context'
 
-interface IAuth {
-  auth: boolean
-  setAuth: (c: boolean) => void
-}
 export const AuthContext = createContext<IAuth>({
   auth: false,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setAuth: () => {}
 })
+export const LoadingContext = createContext<ILoading>({
+  loading: false,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setLoading: () => {}
+})
 
 const App: FC = () => {
   const [auth, setAuth] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
-      <Routes>
-        <Route path="*" element={<Navigate to={SIGN_IN} />} />
-        <Route path="/" element={<MainLayout />}>
-          {publicRoutes.map(({ path, Component }) => (
-            <Route key={path} path={path} element={<Component />} />
-          ))}
-          {!auth && <Route path="*" element={<Navigate to={SIGN_IN} />} />}
-        </Route>
-      </Routes>
+      <LoadingContext.Provider value={{ loading, setLoading }}>
+        <Routes>
+          <Route path="*" element={<Navigate to={SIGN_IN} />} />
+          <Route path="/" element={<MainLayout />}>
+            {publicRoutes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+            {!auth && <Route path="*" element={<Navigate to={SIGN_IN} />} />}
+          </Route>
+        </Routes>
+      </LoadingContext.Provider>
     </AuthContext.Provider>
   )
 }
--- front/src/layouts/MainLayout.tsx
+++ front/src/layouts/MainLayout.tsx
@@ -1,14 +1,18 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import { Outlet } from 'react-router-dom'
+import { LoadingContext } from '../App'
 import Header from '../components/header'
+import Spinner from '../components/spinner'
 
 const MainLayout: FC = () => {
+  const { loading } = useContext(LoadingContext)
   return (
     <>
       <Header />
       <main className="main">
         <Outlet />
       </main>
+      {loading && <Spinner />}
     </>
   )
 }
