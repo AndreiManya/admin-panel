db986fb6231de4f5e1bc0477f282e6d2e0a1086b
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-07 20:33:18 +0300

    edit: user type

--- front/src/@types/user.ts
+++ front/src/@types/user.ts
@@ -1,4 +1,14 @@
-export default interface IUser {
+export interface IUser {
   token: string
+  id: number
+}
+
+export interface IUsersData {
+  id: React.Key
   name: string
+  email: string
+  password: string
+  date: Date
+  lastLogin: Date
+  status: string
 }
