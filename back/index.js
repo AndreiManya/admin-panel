5166e03b751e836e0772e56025a12e58606c6dde
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-07 07:40:28 +0300

    feat: configure server

---
+++ back/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import router from './routes/user.routes.js'
+
+const PORT = 8080
+
+const app = express()
+app.use(express.json())
+app.use('/api', router)
+
+app.listen(PORT, () => console.log('Server started'))
