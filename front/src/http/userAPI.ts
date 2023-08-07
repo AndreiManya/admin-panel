1b54f02d4e6dbc7a9938c57ec1ccc4a8ea5b71cc
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-08-07 20:33:32 +0300

    edit: apis

--- front/src/http/userAPI.ts
+++ front/src/http/userAPI.ts
@@ -1,20 +1,30 @@
 import { $authHost, $host } from './index'
-import IUser from '../@types/user'
+import { IUser, IUsersData } from '../@types/user'
+import { Key } from 'react'
 
 export const login = async (name: string, password: string): Promise<IUser> => {
   const { data } = await $host.post<IUser>('login', { name, password })
   localStorage.setItem('token', data.token)
-  localStorage.setItem('user', JSON.stringify(data.name))
+  localStorage.setItem('user', JSON.stringify(data.id))
   return data
 }
 
-export const register = async (
-  username: string,
-  password: string,
-  email: string
-): Promise<IUser> => {
-  const { data } = await $host.post<IUser>('register', { username, password, email })
-  localStorage.setItem('token', data.token)
-  localStorage.setItem('user', JSON.stringify(data.name))
+export const register = async (name: string, password: string, email: string): Promise<IUser> => {
+  const { data } = await $host.post<IUser>('register', { name, password, email })
+  return data
+}
+
+export const getUsers = async (): Promise<IUsersData[]> => {
+  const { data } = await $authHost.get<IUsersData[]>('users')
+  return data
+}
+
+export const changeStatus = async (users: Key[], status: string): Promise<string> => {
+  const { data } = await $authHost.put<string>('users', { users, status })
+  return data
+}
+
+export const deleteUsers = async (users: Key[]): Promise<Key> => {
+  const { data } = await $authHost.delete<Key>('users', { data: { users } })
   return data
 }
